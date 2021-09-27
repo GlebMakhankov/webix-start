2e04f82fbe2febbc0758ba359de3dddb93335918
= <=>
2021-09-28 00:45:59 +0300

    fix: invalid argument

--- src/functions/GetRandomUser.js
+++ src/functions/GetRandomUser.js
@@ -8,5 +8,5 @@ export default function getRandomUser() {
     { name: "Sam Smith", age: 20, country: "Japan" },
     { name: "Frank Pinata", age: 87, country: "Italy" },
   ];
-  return users[getRandomIntInclusive(0, users.length)];
+  return users[getRandomIntInclusive(0, users.length - 1)];
 }
